c173925c254a3f77f90d85298fc1eb0c779c6585
Isaacdvs2 <dvs2mona@gmail.com>
2023-02-07 13:02:10 +0000

    edit base file, auth gen file, test for base file

--- models/base_model.py
+++ models/base_model.py
@@ -2,42 +2,39 @@
 
 from uuid import uuid4
 from datetime import datetime
-from models import storage
 
 class BaseModel:
-"""
-The BaseModel class defines the common attributes/methods for other classes.
+    """
+        The BaseModel class defines the common attributes/methods for other classes.
 
-Public instance attributes:
-	- id : string - assign with an uuid when an instance is created. The
+        Public instance attributes:
+	    - id : string - assign with an uuid when an instance is created. The
 	        uuid must be unique for every instance.
-	- created_at: datetime - assign with the current datetime when an instace
+	    - created_at: datetime - assign with the current datetime when an instace
                 is created.
-	- updated_at: datetime - assign with the current datetime when an instance 
+	    - updated_at: datetime - assign with the current datetime when an instance 
                 is created and it will be updated every time you change your object.
 
-Public instance methods: save(), to_dict()
-"""
+        Public instance methods: save(), to_dict()
+     """
 
     def __init__(self, *args, **kwargs):
         """
          The init method is the constructor for the base model
         """
         self.id = str(uuid4())
-	self.created_at = datetime.now()
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """returns the string representation of the base model object in the form:
            [<class name>] (<self.id>) <self.__dict__>
         """
-        return ("[{}] ({}) {}".format(self.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__))
 
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime """
-	self.updated_at = datetime.now()
-        storage.save()
-        return 0
+        self.updated_at = datetime.now()
     
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance object
@@ -48,9 +45,9 @@ Public instance methods: save(), to_dict()
 	* Running the __dict__ method on the object helps understand this task * 
         """
         obj_dict = self.__dict__.copy()
-        obj_dict.__class__ = self.__class__.__name__
+        obj_dict["__class__"] = self.__class__.__name__
         
         for key, value in self.__dict__.items():
             if key in ("created_at", "updated_at"):
                 obj_dict = self.__dict__[key].isoformat()
-        return 0
+        return obj_dict

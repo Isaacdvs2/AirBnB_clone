bed64c0f3fa228d5653ac42d6914c1184617200d
Isaacdvs2 <dvs2mona@gmail.com>
2023-02-09 18:03:58 +0000

    created file storage class, updated base model and init file

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """
@@ -27,6 +28,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
         else:
             for key, value in kwargs.items():
@@ -45,6 +47,7 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at with the current datetime """
         self.updated_at = datetime.now()
+        storage.save()
     
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance object

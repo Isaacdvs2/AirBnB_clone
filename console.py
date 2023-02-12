a80ec2649a3e537b6d76b0cdf1c1bf2872fadfae
Isaacdvs2 <dvs2mona@gmail.com>
2023-02-12 09:07:47 +0000

    updated parse and check args method

--- console.py
+++ console.py
@@ -4,6 +4,41 @@ import cmd
 
 """ The console program for the project """
 
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
+
+def check_args(args):
+    """checks if args is valid
+
+    Args:
+        args (str): the string containing the arguments passed to a command
+
+    Returns:
+        Error message if args is None or not a valid class, else the arguments
+    """
+    arg_list = parse(args)
+
+    if len(arg_list) == 0:
+        print("** class name missing **")
+    elif arg_list[0] not in CLASSES:
+        print("** class doesn't exist **")
+    else:
+        return arg_list
+
 class HBNBCommand(cmd.Cmd):
 
     """
@@ -21,6 +56,10 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
 
+    def do_create(self, argv):
+        """ Creates a new instance of BaseModel, saves it (to the JSON file)
+         and prints the id"""
+        
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
